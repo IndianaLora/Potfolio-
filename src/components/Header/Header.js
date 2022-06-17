4797619a2b1ec20768984c376e557fc2956b03de
Indiana Lora <indiana@ensaludybelleza.com>
2022-06-17 11:18:00 -0400

    style the header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import "./header.css"
 import Info from './Info'
-import Me from "../../assest/me.jfif"
 function Header() {
   return (
     <header>
       <div class="container">
         <div class="container header-container">
-          <h5>Hello I'm</h5>
+          <h3>Hello I'm</h3>
           <h1>Indiana Lora</h1>
-          <h5 className='text-light'>Frontend Developer</h5>
+          <h2 className='text-light'>Frontend Developer</h2>
           <Info />
-          <div class="me">
-            <img src={Me} alt='Myself' />
-            <a href="#contact">Scroll Down</a>
-          </div>
         </div>
       </div>
     </header>
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,7 +2,10 @@ import React from 'react'
 import "./nav.css"
 function Nav() {
   return (
-    <div>Nav</div>
+
+    <div className='nav'>Nav</div>
+
+
   )
 }
 
