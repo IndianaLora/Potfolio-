469918065e4321237c34774726853571faec17cb
Indiana Lora <indiana@ensaludybelleza.com>
2022-06-27 11:01:16 -0400

    Updates

---
+++ precache-manifest.edb6021434eb62b7ac3117609d003e82.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "14602dd2942ed28cf7a2",
+    "url": "/static/css/main.2dd3a986.chunk.css"
+  },
+  {
+    "revision": "14602dd2942ed28cf7a2",
+    "url": "/static/js/main.6d43cc88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36e65246669672a27fd3",
+    "url": "/static/js/2.efa8c155.chunk.js"
+  },
+  {
+    "revision": "37627d038cee4056392231c1f852c04a",
+    "url": "/static/media/programmer-bg.37627d03.jpg"
+  },
+  {
+    "revision": "925dbabd8dcd9939038ce460f5b57c97",
+    "url": "/static/media/Indiana 2022 Resume.925dbabd.pdf"
+  },
+  {
+    "revision": "2da23668ef58dc423eb885dace922723",
+    "url": "/static/media/me.2da23668.jfif"
+  },
+  {
+    "revision": "e75142f724449fbdd6a845eb565bc1a3",
+    "url": "/static/media/shine.e75142f7.gif"
+  },
+  {
+    "revision": "fa93836d5022f20a45ec6ac582053297",
+    "url": "/static/media/LyricsSearcher.fa93836d.png"
+  },
+  {
+    "revision": "bb8837ab9b0b3ca878b9ba223f49cc12",
+    "url": "/static/media/ProductivityManager.bb8837ab.png"
+  },
+  {
+    "revision": "2cc6e56759f09b11397d823e7b44d88a",
+    "url": "/static/media/Notebook.2cc6e567.png"
+  },
+  {
+    "revision": "40ff19d9defc394644def7e03e83b69e",
+    "url": "/static/media/ResinPage.40ff19d9.png"
+  },
+  {
+    "revision": "be0e73ac18e3ddd9c1223867189aa8b0",
+    "url": "/static/media/plamTree.be0e73ac.jfif"
+  },
+  {
+    "revision": "ab8ce57ee7f779919b30a5e9099df59d",
+    "url": "/index.html"
+  }
+];
