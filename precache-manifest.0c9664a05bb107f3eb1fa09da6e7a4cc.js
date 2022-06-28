23f5bcb7d024968b1e8e1bd81821af7b4a0904fb
Indiana Lora <indiana@ensaludybelleza.com>
2022-06-28 09:22:06 -0400

    Updates

---
+++ precache-manifest.0c9664a05bb107f3eb1fa09da6e7a4cc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0fb5e8adea9ee3313f3",
+    "url": "/portfoliio/static/css/main.e87dbe9d.chunk.css"
+  },
+  {
+    "revision": "d0fb5e8adea9ee3313f3",
+    "url": "/portfoliio/static/js/main.d0fb5e8a.chunk.js"
+  },
+  {
+    "revision": "f85eb0f1622ac54e5aa6",
+    "url": "/portfoliio/static/js/1.f85eb0f1.chunk.js"
+  },
+  {
+    "revision": "2d4b2d5cc4eb0fdb3c5e",
+    "url": "/portfoliio/static/js/runtime~main.2d4b2d5c.js"
+  },
+  {
+    "revision": "37627d038cee4056392231c1f852c04a",
+    "url": "/portfoliio/static/media/programmer-bg.37627d03.jpg"
+  },
+  {
+    "revision": "925dbabd8dcd9939038ce460f5b57c97",
+    "url": "/portfoliio/static/media/Indiana 2022 Resume.925dbabd.pdf"
+  },
+  {
+    "revision": "2da23668ef58dc423eb885dace922723",
+    "url": "/portfoliio/static/media/me.2da23668.jfif"
+  },
+  {
+    "revision": "e75142f724449fbdd6a845eb565bc1a3",
+    "url": "/portfoliio/static/media/shine.e75142f7.gif"
+  },
+  {
+    "revision": "fa93836d5022f20a45ec6ac582053297",
+    "url": "/portfoliio/static/media/LyricsSearcher.fa93836d.png"
+  },
+  {
+    "revision": "bb8837ab9b0b3ca878b9ba223f49cc12",
+    "url": "/portfoliio/static/media/ProductivityManager.bb8837ab.png"
+  },
+  {
+    "revision": "2cc6e56759f09b11397d823e7b44d88a",
+    "url": "/portfoliio/static/media/Notebook.2cc6e567.png"
+  },
+  {
+    "revision": "40ff19d9defc394644def7e03e83b69e",
+    "url": "/portfoliio/static/media/ResinPage.40ff19d9.png"
+  },
+  {
+    "revision": "be0e73ac18e3ddd9c1223867189aa8b0",
+    "url": "/portfoliio/static/media/plamTree.be0e73ac.jfif"
+  },
+  {
+    "revision": "c2abe925b62ec3de277c80f8b975188b",
+    "url": "/portfoliio/index.html"
+  }
+];
